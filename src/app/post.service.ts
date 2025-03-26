6db6676bd65a5e6e15a41911f63e435370c537d5
Priya <priya.khandgaure@beyondkey.com>
2025-03-26 17:15:43 +0530

    Resolved data issue on refresh

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -1,45 +1,65 @@
 import { Injectable } from "@angular/core";
 import { Post } from './post.model';
 import { BehaviorSubject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({ providedIn: 'root' })
 export class PostService {
   private listOfPost: Post[] = [];
   private listChanged = new BehaviorSubject<Post[]>([]);
 
+  private firebaseUrl = 'https://live-posts-24b51-default-rtdb.firebaseio.com/posts.json';
+
+  constructor(private http: HttpClient) {
+    this.fetchData(); // Fetch data when service initializes
+  }
+
   getPosts() {
-    console.log('Inside get posts',this.listChanged.asObservable());
     return this.listChanged.asObservable();
   }
 
-  deletePost(index: number) {
-    this.listOfPost.splice(index, 1);
+  setPosts(listOfPost: Post[]) {
+    this.listOfPost = listOfPost || [];
     this.listChanged.next([...this.listOfPost]);
+  }
+
+  fetchData() {
+    this.http.get<Post[]>(this.firebaseUrl).subscribe((posts) => {
+      this.setPosts(posts || []);
+    });
+  }
+
+  saveData() {
+    this.http.put(this.firebaseUrl, this.listOfPost).subscribe(() => {
+      console.log('Data saved to Firebase');
+    });
   }
 
   addPost(post: Post) {
     this.listOfPost.push(post);
     this.listChanged.next([...this.listOfPost]);
+    this.saveData(); // Save to Firebase
   }
 
   updatePost(index: number, post: Post) {
     this.listOfPost[index] = post;
     this.listChanged.next([...this.listOfPost]);
+    this.saveData(); // Save to Firebase
   }
 
-  getPostById(index: number) {
-    return this.listOfPost[index];
+  deletePost(index: number) {
+    this.listOfPost.splice(index, 1);
+    this.listChanged.next([...this.listOfPost]);
+    this.saveData(); // Save to Firebase
   }
 
   likePost(index: number) {
     this.listOfPost[index].numberOfLikes += 1;
     this.listChanged.next([...this.listOfPost]);
+    this.saveData(); // Save to Firebase
   }
 
-  setPosts(listOfPost: Post[]) {
-    console.log("list of post", listOfPost);
-    this.listOfPost = listOfPost;
-    console.log("this--- list of post", this.listOfPost);
-    this.listChanged.next([...this.listOfPost]);
+  getPostById(index: number) {
+    return this.listOfPost[index];
   }
 }
